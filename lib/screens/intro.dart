3768431a9b9f015efbe1e5d766bc3d6b7d0bd6ae
= <76913187+Josoa886@users.noreply.github.com>
2024-07-08 10:28:55 +0300

    feat: Introduction's page

---
+++ lib/screens/intro.dart
@@ -0,0 +1,54 @@
+import 'package:ai_test/api/api.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Intro extends StatefulWidget {
+  const Intro({super.key});
+
+  @override
+  State<Intro> createState() => _IntroState();
+}
+
+class _IntroState extends State<Intro> {
+  @override
+  void initState(){
+    goto();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 8, 85, 148),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to',
+            style: GoogleFonts.poppins(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+            ),
+
+            Text('MyAI',
+            style: GoogleFonts.poppins(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+  Future<void> goto() async{
+    await Future.delayed( const Duration(seconds: 5));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyAI()));
+}
+}
+
