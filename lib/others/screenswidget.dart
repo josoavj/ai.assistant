d9cc8aa72a4dd30880e01e0d7b60d5e32a38bd75
= <76913187+Josoa886@users.noreply.github.com>
2024-07-12 15:48:12 +0300

    style: Updating the structure

---
+++ lib/others/screenswidget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({
+    super.key,
+    required this.text,
+    required this.isFromUser,
+  });
+
+  final String text;
+  final bool isFromUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment:
+          isFromUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 480),
+            decoration: BoxDecoration(
+              color: isFromUser
+                  ? Theme.of(context).colorScheme.primaryContainer
+                  : Theme.of(context).colorScheme.surfaceContainerHighest,
+              borderRadius: BorderRadius.circular(18),
+            ),
+            padding: const EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 20,
+            ),
+            margin: const EdgeInsets.only(bottom: 8),
+            child: MarkdownBody(data: text),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+InputDecoration textFieldDecoration(BuildContext context, String hintText) =>
+    InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: hintText,
+      border: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+    );
